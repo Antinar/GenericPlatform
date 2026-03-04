619f7ce96253fc2ac27a45ab90e7273b919fd4d5
Antinar <antinaraj@gmail.com>
2026-03-04 19:17:15 +0100

    Añadido de vida al jugador y un poco de mapa

--- BasicPlatform/Scripts/Player.cs
+++ BasicPlatform/Scripts/Player.cs
@@ -4,15 +4,24 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	//Constantes
+	//Constanstes de movimiento
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 	public const int NJumps = 2;
 	//Variables privadas 
+	//Variables de Animaciones
 	private AnimatedSprite2D _animatedSprite2D;
+	//Variables de movimiento
 	private int _jumpsleft;
+	//Variables de vida del jugador
+	[Export] public int PlayerMaxHealth = 3;
+	[Export] public float DamageCooldown = 1.0f;
+	private int _playerHealth;
+	private bool _canTakeDamage = true;
 
 	public override void _Ready()
 	{
+		_playerHealth = PlayerMaxHealth;
 		_animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 	}
 
@@ -55,10 +64,10 @@ public partial class Player : CharacterBody2D
 		MoveAndSlide();
 
 		// Lógica de animación
-		ActualizarAnimaciones(direction);
+		AnimacionesMovimiento(direction);
 	}
 
-	private void ActualizarAnimaciones(float direction)
+	private void AnimacionesMovimiento(float direction)
 	{
 		if (!IsOnFloor())
 		{
@@ -78,5 +87,54 @@ public partial class Player : CharacterBody2D
 		{
 			_animatedSprite2D.FlipH = direction < 0;
 		}
+
+	}
+
+	private void AnimacionesVida(int opción)
+	{
+		//El jugador a sido dañado
+		if (opción == 1)
+		{
+			_animatedSprite2D.Play("damaged");
+		}//El jugador a muerto
+		else if (opción == 2)
+		{
+			_animatedSprite2D.Play("Death");
+		}
+	}
+
+	public void TakeDamage(int damage)
+	{
+		if (!_canTakeDamage) return;
+
+		_playerHealth -= damage;
+		_playerHealth = Mathf.Clamp(_playerHealth, 0, PlayerMaxHealth);
+
+		if (_playerHealth <= 0)
+		{
+			Die();
+		}
+		else
+		{
+			Invulnerability();
+		}
+	}
+
+	private async void Invulnerability()
+	{
+		_canTakeDamage = false;
+
+		Modulate = new Color(1,1,1,0.5f);
+
+		await ToSignal(GetTree().CreateTimer(DamageCooldown), SceneTreeTimer.SignalName.Timeout);;
+		_canTakeDamage = true;
+        Modulate = new Color(1, 1, 1, 1);
+		AnimacionesVida(1);
+	}
+	private void Die()
+	{	
+		AnimacionesVida(2);
+		//PlaceHolder (reinicia la escena)
+		GetTree().ReloadCurrentScene();
 	}
 }
