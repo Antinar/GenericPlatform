f96d5fb657e433481c59661b9f1ed131e31df35a
Antinar <antinaraj@gmail.com>
2026-03-04 13:48:02 +0100

    Añadido de fondo provisional

--- BasicPlatform/Scripts/Player.cs
+++ BasicPlatform/Scripts/Player.cs
@@ -3,9 +3,13 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	//Constantes
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+	public const int NJumps = 2;
+	//Variables privadas 
 	private AnimatedSprite2D _animatedSprite2D;
+	private int _jumpsleft;
 
 	public override void _Ready()
 	{
@@ -21,11 +25,18 @@ public partial class Player : CharacterBody2D
 		{
 			velocity += GetGravity() * (float)delta;
 		}
+		else
+		{
+			_jumpsleft = NJumps;
+		}
 
 		// Lógica de Salto
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("ui_accept") && (IsOnFloor() || _jumpsleft > 0))
 		{
+
 			velocity.Y = JumpVelocity;
+			_jumpsleft--;
+
 		}
 
 		// Obtenemos la dirección (float entre -1 y 1)
@@ -42,7 +53,7 @@ public partial class Player : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
-		
+
 		// Lógica de animación
 		ActualizarAnimaciones(direction);
 	}
@@ -61,7 +72,7 @@ public partial class Player : CharacterBody2D
 		{
 			_animatedSprite2D.Play("idle");
 		}
-		
+
 		// Voltear el sprite
 		if (direction != 0)
 		{
